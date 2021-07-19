5c24081b6986bdf3cc9410fac89ba12dacab173f
EvilCalf <xujiading1997@163.com>
2021-07-19 16:27:06 +0800

    update label

--- test.py
+++ test.py
@@ -12,35 +12,39 @@ model_path='model/model.h5'
 vocab_path='model/vocab.txt'
 test_path='test/input.txt'
 class_dict = {
-            "O": 0,
-            "B-DISEASE": 1,
-            "I-DISEASE": 2,
-            "B-SIGN": 3,
-            "I-SIGN": 4,
-            "B-MARGIN": 5,
-            "I-MARGIN": 6,
-            "B-DIAMETER": 7,
-            "I-DIAMETER": 8,
-            "B-TESTPROC": 9,
-            "I-TESTPROC": 10,
-            "B-TREATMENT": 11,
-            "I-TREATMENT": 12,
-            "B-ANATOMY": 13,
-            "I-ANATOMY": 14,
-            "B-NATURE": 15,
-            "I-NATURE": 16,
-            "B-SHAPE": 17,
-            "I-SHAPE": 18,
-            "B-DENSITY": 19,
-            "I-DENSITY": 20,
-            "B-BOUNDARY": 21,
-            "I-BOUNDARY": 22,
-            "B-LUNGFIELD": 23,
-            "I-LUNGFIELD": 24,
-            "B-TEXTURE": 25,
-            "I-TEXTURE": 26,
-            "B-TRANSPARENCY": 27,
-            "I-TRANSPARENCY": 28
+            'O': 0,  # 非实体
+            'B-DISEASE': 1,  # 疾病
+            'I-DISEASE': 2,
+            'B-SIGN': 3,  # 异常检查结果
+            'I-SIGN': 4,
+            'B-MARGIN': 5,  # 边缘
+            'I-MARGIN': 6,
+            'B-DIAMETER': 7,  # 直径
+            'I-DIAMETER': 8,
+            'B-TESTPROC': 9,  # 检查过程
+            'I-TESTPROC': 10,
+            'B-TREATMENT': 11,  # 治疗过程
+            'I-TREATMENT': 12,
+            'B-ORGAN': 13,  # 器官
+            'I-ORGAN': 14,
+            'B-ANATOMY': 15,  # 部位
+            'I-ANATOMY': 16,
+            'B-NATURE': 17,  # 性质
+            'I-NATURE': 18,
+            'B-SHAPE': 19,  # 形状
+            'I-SHAPE': 20,
+            'B-DENSITY': 21,  # 密度
+            'I-DENSITY': 22,
+            'B-BOUNDARY': 23,  # 边界
+            'I-BOUNDARY': 24,
+            'B-LUNGFIELD': 25,  # 肺野
+            'I-LUNGFIELD': 26,
+            'B-TEXTURE': 27,  # 纹理
+            'I-TEXTURE': 28,
+            'B-TRANSPARENCY': 29,  # 透明度
+            'I-TRANSPARENCY': 30,
+            'B-QUANTITY': 31,  # 数量
+            'I-QUANTITY': 32
         }
 maxLen=198
 classSum=29
--- transfer_sict.py
+++ transfer_sict.py
@@ -4,54 +4,43 @@ import csv
 import json
 import jsonlines
 
+
 class TransferData:
     def __init__(self):
-        self.label_dict = {
-            "疾病": "DISEASE",
-            "异常检查结果": "SIGN",
-            "边缘": "MARGIN",
-            "直径": "DIAMETER",
-            "检查过程": "TESTPROC",
-            "治疗过程": "TREATMENT",
-            "部位": "ANATOMY",
-            "性质": "NATURE",
-            "形状": "SHAPE",
-            "密度": "DENSITY",
-            "边界": "BOUNDARY",
-            "肺野": "LUNGFIELD",
-            "纹理": "TEXTURE",
-            "透亮度": "TRANSPARENCY"
-        }
         self.cate_dict = {
-            "O": 0,
-            "DISEASE-B": 1,
-            "DISEASE-I": 2,
-            "SIGN-B": 3,
-            "SIGN-I": 4,
-            "MARGIN-B": 5,
-            "MARGIN-I": 6,
-            "DIAMETER-B": 7,
-            "DIAMETER-I": 8,
-            "TESTPROC-B": 9,
-            "TESTPROC-I": 10,
-            "TREATMENT-B": 11,
-            "TREATMENT-I": 12,
-            "ANATOMY-B": 13,
-            "ANATOMY-I": 14,
-            "NATURE-B": 15,
-            "NATURE-I": 16,
-            "SHAPE-B": 17,
-            "SHAPE-I": 18,
-            "DENSITY-B": 19,
-            "DENSITY-I": 20,
-            "BOUNDARY-B": 21,
-            "BOUNDARY-I": 22,
-            "LUNGFIELD-B": 23,
-            "LUNGFIELD-I": 24,
-            "TEXTURE-B": 25,
-            "TEXTURE-I": 26,
-            "TRANSPARENCY-B": 27,
-            "TRANSPARENCY-I": 28
+            'O': 0,  # 非实体
+            'B-DISEASE': 1,  # 疾病
+            'I-DISEASE': 2,
+            'B-SIGN': 3,  # 异常检查结果
+            'I-SIGN': 4,
+            'B-MARGIN': 5,  # 边缘
+            'I-MARGIN': 6,
+            'B-DIAMETER': 7,  # 直径
+            'I-DIAMETER': 8,
+            'B-TESTPROC': 9,  # 检查过程
+            'I-TESTPROC': 10,
+            'B-TREATMENT': 11,  # 治疗过程
+            'I-TREATMENT': 12,
+            'B-ORGAN': 13,  # 器官
+            'I-ORGAN': 14,
+            'B-ANATOMY': 15,  # 部位
+            'I-ANATOMY': 16,
+            'B-NATURE': 17,  # 性质
+            'I-NATURE': 18,
+            'B-SHAPE': 19,  # 形状
+            'I-SHAPE': 20,
+            'B-DENSITY': 21,  # 密度
+            'I-DENSITY': 22,
+            'B-BOUNDARY': 23,  # 边界
+            'I-BOUNDARY': 24,
+            'B-LUNGFIELD': 25,  # 肺野
+            'I-LUNGFIELD': 26,
+            'B-TEXTURE': 27,  # 纹理
+            'I-TEXTURE': 28,
+            'B-TRANSPARENCY': 29,  # 透明度
+            'I-TRANSPARENCY': 30,
+            'B-QUANTITY': 31,  # 数量
+            'I-QUANTITY': 32
         }
         self.entity_dirpath = "data/sict"
         self.train_filepath = "train/sict_train.txt"
@@ -66,13 +55,13 @@ class TransferData:
                 #     data = list(reader)
                 # if len(data) == 0:
                 #     continue
-                data=[]
+                data = []
                 with open(root+"/"+file, "r+", encoding="utf8") as f1:
                     for item in jsonlines.Reader(f1):
                         data.append(item)
-                for i,strs in enumerate(data):
+                for i, strs in enumerate(data):
                     res_dict = {}
-                    for j,raw in enumerate(strs['label']):
+                    for j, raw in enumerate(strs['label']):
                         start = int(raw[0])
                         end = int(raw[1])
                         label_id = raw[2]
